5bd6076614563e458bcbe1f652704089e18b11fe
Kevin Camidge <kevincamidge@gmail.com>
2017-03-30 23:24:29 -0400

    Cleaned up variable names

--- xmlutil.py
+++ xmlutil.py
@@ -5,17 +5,19 @@ import shutil, os
 from xml.dom import minidom
 
 class XMLFile:
-    def __init__(self, filename, mydir):
-        self.myfilename = filename
-        self.mydir = mydir
-        self.myfilepath = mydir + filename
-        self.mp3filepathlist = []
+    def __init__(self, fileName, dest):
+        self.fileName = fileName
+        self.dest = dest
+        self.filePath = dest + fileName
+        self.iTunesFilePathList = []
+        self.destinationFilePathList = []
         self.parseFileForLocations()
 
+    # Searches the xml for the file locations
     def parseFileForLocations(self):
-        # minidom.parse("filepath to xml file")
-        xmldoc = minidom.parse(self.myfilepath)
-        plist = xmldoc.getElementsByTagName("plist")[0]
+        # minidom.parse("filePath to xml file")
+        xmlDoc = minidom.parse(self.filePath)
+        plist = xmlDoc.getElementsByTagName("plist")[0]
         dict1 = plist.getElementsByTagName("dict")[0]
         dict2 = dict1.getElementsByTagName("dict")[0]
         dict3 = dict2.getElementsByTagName("dict")
@@ -24,30 +26,31 @@ class XMLFile:
             for x in key:
                 text = x.firstChild.data
                 if (text == "Location"):
-                    filepathfull = x.nextSibling.firstChild.data
-                    filepath = filepathfull[17:]
-                    filepath = getAbsFilepath(filepath)
-                    self.mp3filepathlist.append(filepath)
-
-    def getFilepathList(self):
-        return self.mp3filepathlist
+                    filePathFull = x.nextSibling.firstChild.data
+                    filePath = filePathFull[17:]
+                    filePath = getAbsFilePath(filePath)
+                    self.destinationFilePathList.append(self.dest + filePath[16:])
+                    self.iTunesFilePathList.append(filePath)
 
 
-#### This block deals with converting the xml filepath into a usable windows filepath
-def replace_chars(filepath):
-    filepath = filepath.replace("%20"," ")
-    filepath = filepath.replace("%C3%A9","é")
-    filepath = filepath.replace("%C3%A1","á")
-    return filepath
-def getAbsFilepath(filepath):
-    absfilepath = os.path.abspath(filepath)
-    absfilepath = replace_chars(absfilepath)
-    return absfilepath
-def getAbsFilepathList(list):
-    newlist = []
+
+
+
+#### This block deals with converting the xml filePath into a usable windows filePath
+def replace_chars(filePath):
+    filePath = filePath.replace("%20"," ")
+    filePath = filePath.replace("%C3%A9","é")
+    filePath = filePath.replace("%C3%A1","á")
+    return filePath
+def getAbsFilePath(filePath):
+    absFilePath = os.path.abspath(filePath)
+    absFilePath = replace_chars(absFilePath)
+    return absFilePath
+def getAbsFilePathList(list):
+    new_list = []
     for path in list:
-        newlist.append(getAbsFilepath(path))
-    return newlist
+        new_list.append(getAbsFilePath(path))
+    return new_list
 ####
 
 
@@ -56,7 +59,7 @@ def addFiles(new_xml, dest_dir):
     if not os.path.isfile(dest_dir + "OldPlaylist.xml"):
         #deleteAll(dest_dir)
         addAll(new_xml, dest_dir)
-        shutil.copy(new_xml.filepath, dest_dir + "OldPlaylist.xml")
+        shutil.copy(new_xml.filePath, dest_dir + "OldPlaylist.xml")
         print("All files have been copied to the desired location.")
         return
     else:
@@ -64,40 +67,39 @@ def addFiles(new_xml, dest_dir):
         print("Nothing happens")
 
 
-
-# Checks if $filepath is present in $xmldoc
-def isPresent(filepath, xmldoc):
-    filepathlist = xmldoc.mp3filepathlist
-    for path in filepathlist:
-        if(path == filepath):
+# Checks if $filePath is present in $xmlDoc
+def isPresent(filePath, xmlDoc):
+    filePathList = xmlDoc.iTunesFilePathList
+    for path in filePathList:
+        if(path == filePath):
             return 1
     return 0
 
 
-# Delete files present in $old_xmldoc that are not present in $new_xmldoc			
-def deleteRemovedFiles(old_xmldoc, new_xmldoc, location):
+# Delete files present in $old_xmlDoc that are not present in $new_xmlDoc
+def deleteRemovedFiles(old_xmlDoc, new_xmlDoc, location):
     present = 0
-    old_list = old_xmldoc.mp3filepathlist
-    new_list = new_xmldoc.mp3filepathlist
+    old_list = old_xmlDoc.iTunesFilePathList
+    new_list = new_xmlDoc.iTunesFilePathList
     for path in old_list:
         if(path not in new_list):
             # remove new file location, not itunes location
             os.remove(path)
 
 
-# Adds files present in $new_xmldoc that are not present in $old_xmldoc
-def addNewFiles(old_xmldoc, new_xmldoc, dest):
-    old_list = old_xmldoc.mp3filepathlist
-    new_list = new_xmldoc.mp3filepathlist
+# Adds files present in $new_xmldoc that are not present in $old_xmlDoc
+def addNewFiles(old_xmlDoc, new_xmlDoc, dest):
+    old_list = old_xmlDoc.iTunesFilePathList
+    new_list = new_xmlDoc.iTunesFilePathList
     for path in new_list:
         if(path not in old_list):
-            shutil.copy(getAbsFilepath(path), dest)
+            shutil.copy(getAbsFilePath(path), dest)
     return 0
 
 
-# Adds all files from $xmldoc
-def addAll(xmldoc, dest):
-    filepathlist = xmldoc.getFilepathList()
-    absfilepathlist = getAbsFilepathList(filepathlist)
-    for filepath in absfilepathlist:
-        shutil.copy(filepath, dest)
+# Adds all files from $xmlDoc
+def addAll(xmlDoc, dest):
+    filePathList = xmlDoc.iTunesFilePathList
+    absFilePathList = getAbsFilePathList(filePathList)
+    for filePath in absFilePathList:
+        shutil.copy(filePath, dest)
